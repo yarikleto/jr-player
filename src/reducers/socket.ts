72d581bcf6798e97df46f97fd3c760f4669ea6b8
alfimois <alfimois@gmail.com>
2017-11-08 22:39:22 +0300

    delete some music and fix socket

--- src/reducers/socket.ts
+++ src/reducers/socket.ts
@@ -3,7 +3,6 @@ import * as io from 'socket.io-client'
 const HOST = 'http://localhost:9100'
 
 export function socket(state: SocketIOClient.Socket = io(HOST), action: any) {
-  console.log(state);
   return state
 }
 
